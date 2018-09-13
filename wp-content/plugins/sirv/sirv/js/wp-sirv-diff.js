6189882b933419bf502ba8dc0f6d30c81a525eaf
Lee Yunseok <5509466+ricke00@users.noreply.github.com>
2018-09-13 19:58:33 +0900

    Edit

--- wp-content/plugins/sirv/sirv/js/wp-sirv-diff.js
+++
@@ -1,5 +0,0 @@
-jQuery( document ).ajaxComplete(function() {
-    if (arguments[1].responseText && arguments[1].responseText.match(/class *= *"[^"]*Sirv/gm)) {
-        setTimeout(function(){Sirv.start();},100);
-    }
-});
